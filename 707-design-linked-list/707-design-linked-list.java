e1be7294d6d5fb58e7ad8d6ed3d44b9b1da568f3
ChsPranav <86569774+ChsPranav@users.noreply.github.com>
2022-03-13 09:32:28 +0530

    Time: 8 ms (88.38%), Space: 51.4 MB (7.21%) - LeetHub

---
+++ 707-design-linked-list/707-design-linked-list.java
@@ -0,0 +1,84 @@
+class MyLinkedList {
+    Node head, tail;
+    private class Node {
+        int data;
+        Node next;
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    
+    public MyLinkedList() {
+        head = tail = null;
+    }
+    
+    public int get(int index) {
+        if(head == null) return -1;
+        Node node = head;
+        while(node != null && index-- > 0) {
+            node = node.next;
+        }
+        return node == null ? -1 : node.data;
+    }
+    
+    public void addAtHead(int val) {
+       if(head == null) {
+           head = new Node(val);
+           tail = head;
+       } else {
+           Node node = new Node(val);
+           node.next = head;
+           head = node;
+       }
+    }
+    
+    public void addAtTail(int val) {
+        if(head == null) {
+            head = new Node(val);
+            tail = head;
+        } else {
+            tail.next = new Node(val);
+            tail = tail.next;
+        }
+    }
+    
+    public void addAtIndex(int index, int val) {
+        if(index == 0) {
+            addAtHead(val);
+            return;
+        }
+        Node node = head;
+        while(node != null && index-- > 1) {
+            node = node.next;
+        }
+        
+        if(node == null) return;
+        
+        Node next = node.next;
+        Node newNode = new Node(val);
+        node.next = newNode;
+        newNode.next = next;
+        if(next == null) tail = newNode;
+        
+    }
+    
+    public void deleteAtIndex(int index) {
+        if(head == null) return;
+        if(index == 0) {
+            head = head.next;
+            if(head == null) tail = null;
+            return;
+        }
+        Node node = head;
+        while(node != null && index-- > 1) {
+            node = node.next;
+        }
+        
+        if(node == null || node.next == null) return;
+        node.next = node.next.next;
+        if(node.next == null) {
+            tail = node;
+        }
+    }
+}

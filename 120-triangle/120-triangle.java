bc091b47a9bbb228cada78798f916917361a6cdd
ChsPranav <86569774+ChsPranav@users.noreply.github.com>
2022-08-01 23:54:13 +0530

    Time: 9 ms (23.15%), Space: 44.5 MB (25.17%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,33 +1,26 @@
 class Solution {
-    int dp[][];
     public int minimumTotal(List<List<Integer>> triangle) {
-        dp=new int[triangle.size()][triangle.get(triangle.size()-1).size()];
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<dp[0].length;j++){
-                dp[i][j]=-1;
-            }
-        }
-        return rec(triangle,0,0);
-    }
-    public int rec(List<List<Integer>> triangle,int index,int pos){
-        if(index==triangle.size())return 0;
-        List<Integer>data=new ArrayList<>(triangle.get(index));
-        if(dp[index][pos]!=-1)return dp[index][pos];
-        int min;
-        if(index+1!=triangle.size()){
-            if(pos+1!=data.size()){
-                min=Math.min(rec(triangle,index+1,pos)+data.get(pos),rec(triangle,index+1,pos+1)+data.get(pos+1));
+        //int min=Integer.MAX_VALUE;
+        List<List<Integer>>ans=new ArrayList<>();
+        List<Integer>data =new ArrayList<>();
+        for(int i=triangle.size()-1;i>=0;i--){
+            if(i==triangle.size()-1){
+                int value=i;
+                for(int j=0;j<triangle.get(i).size();j++){
+                    data.add(triangle.get(i).get(j));
+                }
+                ans.add(new ArrayList<>(data));
             }
             else{
-                min=rec(triangle,index+1,pos)+data.get(pos);
+                for(int j=0;j<triangle.get(i).size();j++){
+                    int temp=Math.min(data.get(j),data.get(j+1));
+                    //System.out.println(j+"and values was"+triangle.get(i).get(j)+"also"+temp);
+                    data.set(j,triangle.get(i).get(j)+temp);
+                }
+                ans.add(new ArrayList<>(data));
             }
         }
-        
-        else{
-            if(pos+1!=data.size())min=Math.min(data.get(pos),data.get(pos+1));
-            else return dp[index][pos]=data.get(pos);
-        }
-        //System.out.println(min+"dsd"+index+"dsds"+pos);
-        return dp[index][pos]=min;
+        //System.out.println(ans);
+        return  data.get(0);
     }
 }

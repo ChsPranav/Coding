f4f56f53ef292a0e6a3a7efbf00b405ee0174385
ChsPranav <86569774+ChsPranav@users.noreply.github.com>
2022-08-01 23:54:44 +0530

    Time: 8 ms (28.31%), Space: 44.3 MB (36.39%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,7 +1,5 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        //int min=Integer.MAX_VALUE;
-        List<List<Integer>>ans=new ArrayList<>();
         List<Integer>data =new ArrayList<>();
         for(int i=triangle.size()-1;i>=0;i--){
             if(i==triangle.size()-1){
@@ -9,7 +7,6 @@ class Solution {
                 for(int j=0;j<triangle.get(i).size();j++){
                     data.add(triangle.get(i).get(j));
                 }
-                ans.add(new ArrayList<>(data));
             }
             else{
                 for(int j=0;j<triangle.get(i).size();j++){
@@ -17,7 +14,6 @@ class Solution {
                     //System.out.println(j+"and values was"+triangle.get(i).get(j)+"also"+temp);
                     data.set(j,triangle.get(i).get(j)+temp);
                 }
-                ans.add(new ArrayList<>(data));
             }
         }
         //System.out.println(ans);

9455023ce5eee5d5b0afe73219b7e54a8037274b
ChsPranav <86569774+ChsPranav@users.noreply.github.com>
2022-09-26 19:30:05 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (62.25%) - LeetHub

--- 66-plus-one/66-plus-one.java
+++ 66-plus-one/66-plus-one.java
@@ -1,51 +1,20 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int temp=digits[digits.length -1];
-        if(temp!=9){
-            digits[digits.length-1]=digits[digits.length-1]+1;
-        }else{
-            if(digits.length!=1){
-                digits[digits.length-1]=0;
-                // digits[digits.length-2]+=1;
-                return check(digits,2);
-            }
-            else{
-                int[] arr=new int[2];
-                arr[0]=1;
-                arr[1]=0;
-                return arr;
-            }
+        int check=1;
+        for(int i=digits.length-1;i>0;i--){
+            int temp=digits[i]+check;
+            digits[i]=temp%10;
+            check=temp/10;
         }
-        return digits;
-    }
-    public static int[] check(int[]digits,int pos){
-        System.out.println(pos);
-        if(digits.length==pos){
-            if(digits[0]==9){
-                int[] arr=new int[digits.length+1];
+        if(check==1 && digits[0]==9){
+            int[] arr=new int[digits.length+1];
             arr[0]=1;
             arr[1]=0;
-            for(int i=1;i<digits.length;i++){
-                arr[i+1]=digits[i];
-            }
+            for(int i=1;i<digits.length;i++)arr[i+1]=digits[i];
             return arr;
-            }
-            else{
-                digits[0]+=1;
-                return digits;
-            }
-            
+        }else{
+            digits[0]+=check;
         }
-        else{
-            if(digits[digits.length-pos]==9){
-                //System.out.println("true");
-                digits[digits.length-pos]=0;
-                return check(digits,++pos);
-            }
-            else{
-                digits[digits.length-pos]+=1;
-                return digits;
-            }
-        }
+        return digits;
     }
 }

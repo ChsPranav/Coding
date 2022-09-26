6cee75840559d13a9d745f17798ed2dd838041a4
ChsPranav <86569774+ChsPranav@users.noreply.github.com>
2022-09-26 11:40:36 +0530

    Time: 9 ms (29.44%), Space: 48.2 MB (17.65%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
@@ -12,12 +12,6 @@ class Solution {
             }
             
         }
-        // for(int i=0;i<matrix.length;i++){
-        //     for(int j=0;j<matrix.length;j++){
-        //         System.out.print(dp[i][j]+" ");
-        //     }
-        //     System.out.println();
-        // }
         int min=dp[matrix.length-1][0];
         for(int i=1;i<matrix.length;i++){
             if(min>dp[matrix.length-1][i])min=dp[matrix.length-1][i];
